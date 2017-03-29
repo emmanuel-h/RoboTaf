e490689297b5bbd75b61ff07e48f60d0a8024b74
Emmanuel H <emmanuelh@openmailbox.org>
2017-03-29 16:03:50 +0200

    Arbre couvrant de poids minimum

--- arbre.erl
+++ arbre.erl
@@ -1,6 +1,8 @@
+-module(arbre).
+-export([calculArbreLeader/1,calculArbreGeneral/1]).
 
 % On calcule les fils du leader dans une fonction à part
-calculArbreLeader(Voisins,Leader) ->
+calculArbreLeader(Voisins) ->
     MessagePere = {pere,self()},
     Taille=length(Voisins),
     % On envoie à tous nos voisins qu'on est le père
@@ -16,18 +18,22 @@ calculArbreLeader(Voisins,Leader) ->
 
 % Fin de la construction de l'arbre
 finConstructionArbre()->
-    ok
+    ok.
 
 % Si tous les fils du leader se sont désactivés c'est bon
 attenteDesactivationLeader(Taille) when Taille == 0 ->
-    ok.
+    io:write('attenteDesactivationLeader Taille = 0\n'),
+    ok;
 
 % Tant que tous les fils du leader ne se sont pas désactivés on attend
 attenteDesactivationLeader(Taille) when Taille > 0 ->
     receive
 	{fils,desactive,FilsUid} ->
+	    io:format("Le fils ~w est désactivé, ma taille est ~w\n",[FilsUid,Taille]),
 	    attenteDesactivationLeader(Taille-1);
-	X -> io:write(X)
+	{pere,PereUid} ->
+	    PereUid ! {fils,nop,self()},
+	    attenteDesactivationLeader(Taille)
     end.
 
 % Quand il n'y a plus de voisins c'est bon
@@ -45,8 +51,8 @@ receptionFilsLeader(Taille) when Taille == 0 ->
 receptionFilsLeader(Taille) when Taille > 0 ->
     receive
 	{fils,ok,FilsUid} ->
-	    receptionFils(Taille-1);
-	X ->io:write(X)
+	    io:format("receptionFilsLeader, FilsUid : ~w, Taille : ~w\n",[FilsUid,Taille]),
+	    receptionFilsLeader(Taille-1)
     end.
 
 % Benoît
@@ -55,16 +61,16 @@ activation(Pere) ->
 
 % Tous nos fils sont désactivés, on ne répond qu'aux processus qui veulent être nos père avec une réponse négative, et on attend de se faire activer pour continuer
 desactive(Pere) ->
+    io:format("~w est désactivé, mon père est ~w\n",[self(),Pere]),
     receive
 	{pere,PereUid} ->
 	    PereUid ! {fils,nop,self()},
 	    desactive(Pere);
-	{activation} -> activation(Pere);
-	X ->io:write(X)
+	{activation} -> activation(Pere)
     end.
 
 % Si on connaît tous nos fils et qu'ils sont tous désactivés, on envoie un message à notre père pour dire qu'on se désactive, puis on se désactive
-receptionfilsGeneral(Fils,Taille,FilsDesactives,Pere) when Taille == 0 and length(Fils) == FilsDesactives ->
+receptionFilsGeneral(Fils,Taille,FilsDesactives,Pere) when ((Taille == 0) and (length(Fils) == FilsDesactives)) ->
     Pere ! {fils,desactive,self()},
     desactive(Pere);
 
@@ -72,11 +78,10 @@ receptionfilsGeneral(Fils,Taille,FilsDesactives,Pere) when Taille == 0 and lengt
 receptionFilsGeneral(Fils,Taille,FilsDesactives,Pere) when Taille == 0 ->
     receive
 	{fils,desactive,IdFils} ->
-	    f2(Fils,Taille,FilsDesactives+1,Pere);
+	    receptionFilsGeneral(Fils,Taille,FilsDesactives+1,Pere);
 	{pere,PereUid} ->
 	    PereUid ! {fils,nop,self()},
-	    f2(Fils,Taille,FilsDesactives,Pere);
-	X ->io:write(X)
+	    receptionFilsGeneral(Fils,Taille,FilsDesactives,Pere)
     end;
 
 % Tant que Taille est supérieur à 0, c'est qu'on n'a pas encore reçu de réponse de tous nos voisins, et qu'on ne sait donc pas encore qui sont nos fils
@@ -84,34 +89,31 @@ receptionFilsGeneral(Fils,Taille,FilsDesactives,Pere) when Taille > 0 ->
     receive
 	% Si on reçoit un refus de parenté, on décrémente Taille
 	{fils,nop,IdFils} ->
-	    f2(Fils,Taille-1,FilsDesactives,Pere);
+	    receptionFilsGeneral(Fils,Taille-1,FilsDesactives,Pere);
 	% Si on reçoit une acceptation de parenté, on décrémente Taille et on ajoute Fils à la liste des fils
 	{fils,ok,IdFils} ->
-	    f2([IdFils|Fils],Taille-1,FilsDesactives,Pere);
+	    receptionFilsGeneral([IdFils|Fils],Taille-1,FilsDesactives,Pere);
 	% Si on reçoit une demande de parenté, on la refuse
 	{pere,PereUid} ->
 	    PereUid ! {fils,nop,self()},
-	    f2(Fils,Taille,FilsDesactives,Pere);
+	    receptionFilsGeneral(Fils,Taille,FilsDesactives,Pere);
 	% Si on reçoit une désactivation d'un de nos fils, on s'en souvient en incrémentant le nombre de fils désactivés
 	{fils,desactive,IdFils} ->
-	    f2(Fils,Taille,FilsDesactives+1,Pere);
-	X ->io:write(X)
+	    receptionFilsGeneral(Fils,Taille,FilsDesactives+1,Pere)
+	%X ->io:write(X)
     end.
 
 % La création de l'arbre pour tous les processus qui ne sont pas le leader
-calculArbreGeneral(Voisins,Leader) ->
+calculArbreGeneral(Voisins) ->
     MessagePere = {pere,self()},
     receive
 	% Activation de notre processus via le message du père
 	{pere,PereUid} ->
 	    % On lui envoie une confirmation de parenté
 	    PereUid ! {fils,ok,self()},
-	    % On supprime notre père de notre liste de fils potentiels
-	    VoisinsSansPere=lists:delete(PereUid,Voisins),
-	    % On envoie à tous nos fils potentiels une demande de parenté
-	    envoiListe(VoisinsSansPere,MessagePere),
-	    Taille=length(VoisinsSansPere),
-	    % On attend la réponse de nos voisins (excepté le père)
+	    envoiListe(Voisins,MessagePere),
+	    Taille=length(Voisins),
+	    % On attend la réponse de nos voisins
 	    receptionFilsGeneral([],Taille,0,PereUid);
 	X ->io:write(X)
     end.
--- structure.erl
+++ structure.erl
@@ -1,27 +1,31 @@
 -module(structure).
--export([start/0,salle/0,salle_debut/0,robot_premier_tour/3,salles_calcul_leader/2]).
+-export([start/0,salle/0,salle_attente_voisins/0,robot_premier_tour/4,salles_calcul_leader/2,salles_calcul_processus_voisins/1,salles_calcul_voisins/3,salle_attente_leader/2]).
 
+% On commence par instancier les 9 salles et les 9 robots
 start() ->
-	register(salle1,spawn(structure,salle_debut,[])),
-	register(salle2,spawn(structure,salle_debut,[])),
-	register(salle3,spawn(structure,salle_debut,[])),
-	register(salle4,spawn(structure,salle_debut,[])),
-	register(salle5,spawn(structure,salle_debut,[])),
-	register(salle6,spawn(structure,salle_debut,[])),
-	register(salle7,spawn(structure,salle_debut,[])),
-	register(salle8,spawn(structure,salle_debut,[])),
-	register(salle9,spawn(structure,salle_debut,[])),
-	register(robot1,spawn(structure,robot_premier_tour,[self(),1,salle1])),
-	register(robot2,spawn(structure,robot_premier_tour,[self(),2,salle2])),
-	register(robot3,spawn(structure,robot_premier_tour,[self(),3,salle3])),
-	register(robot4,spawn(structure,robot_premier_tour,[self(),4,salle4])),
-	register(robot5,spawn(structure,robot_premier_tour,[self(),5,salle5])),
-	register(robot6,spawn(structure,robot_premier_tour,[self(),6,salle6])),
-	register(robot7,spawn(structure,robot_premier_tour,[self(),7,salle7])),
-	register(robot8,spawn(structure,robot_premier_tour,[self(),8,salle8])),
-	register(robot9,spawn(structure,robot_premier_tour,[self(),9,salle9])).
+    Synchroniseur = self(),
+    register(salle1,spawn(structure,salle_attente_voisins,[])),
+    register(salle2,spawn(structure,salle_attente_voisins,[])),
+    register(salle3,spawn(structure,salle_attente_voisins,[])),
+    register(salle4,spawn(structure,salle_attente_voisins,[])),
+    register(salle5,spawn(structure,salle_attente_voisins,[])),
+    register(salle6,spawn(structure,salle_attente_voisins,[])),
+    register(salle7,spawn(structure,salle_attente_voisins,[])),
+    register(salle8,spawn(structure,salle_attente_voisins,[])),
+    register(salle9,spawn(structure,salle_attente_voisins,[])),
+    register(robot1,spawn(structure,robot_premier_tour,[self(),1,salle1,Synchroniseur])),
+    register(robot2,spawn(structure,robot_premier_tour,[self(),2,salle2,Synchroniseur])),
+    register(robot3,spawn(structure,robot_premier_tour,[self(),3,salle3,Synchroniseur])),
+    register(robot4,spawn(structure,robot_premier_tour,[self(),4,salle4,Synchroniseur])),
+    register(robot5,spawn(structure,robot_premier_tour,[self(),5,salle5,Synchroniseur])),
+    register(robot6,spawn(structure,robot_premier_tour,[self(),6,salle6,Synchroniseur])),
+    register(robot7,spawn(structure,robot_premier_tour,[self(),7,salle7,Synchroniseur])),
+    register(robot8,spawn(structure,robot_premier_tour,[self(),8,salle8,Synchroniseur])),
+    register(robot9,spawn(structure,robot_premier_tour,[self(),9,salle9,Synchroniseur])),
+    synchroniseur().
 	
-    
+
+% Permet aux robots de lock une salle tant qu'ils sont dedans
 salle() ->
 	receive
 	    {IdRobot,demande} ->IdRobot!ok,
@@ -31,87 +35,139 @@ salle() ->
 	    fin -> ok		       
 	end.
 
+% Suivant si on est le leader ou pas, on appelle la bonne fonction de construction d'arbre (seul le leader est actif au début)
+salle_attente_leader(Voisins,ListePortes)->
+    receive
+	sync ->
+	    receive
+		{leader,neo} ->
+		    arbre:calculArbreLeader(Voisins);
+		{leader, nop} -> arbre:calculArbreGeneral(Voisins);
+		X -> io:format('Le processus n est ni leader ni dans salle_attente_leader (~w)',[X])
+	    end
+    end.
 
-salle_debut()->
-	receive
-		{portes,ListePortes,IdSalle} -> io:format("~w ~w \n",[self(),ListePortes]),
-					IdSalle ! {portes,ok}, 
-					salle_debut();
-		{leader,neo} -> io:format("je suis leader ~w \n",[self()]),salle();  
-		{leader, nop} -> io:format("je ne suis pas leader ~w \n",[self()]),salle()			
-		
-	
+% Les salles commencent par attendre la liste de leurs voisins sous deux formes : Une liste des directions et une liste des processus
+salle_attente_voisins()->
+    io:format("~w\n",[self()]),
+    receive
+	{voisins,Voisins,ListePortes,IdRobot} ->
+	    IdRobot ! {voisins,ok},
+	    salle_attente_leader(Voisins,ListePortes)
 	end.
 
+% Le robot au premier tour fait un tour complet de la salle pour détecter les portes
+robot_premier_tour(IdRobot,NbRobot,Salle,Synchroniseur) ->
+    R = robotlab:get_robot(NbRobot),
+    O=robotlab:mur(R),
+    S=robotlab:mur(R),
+    E=robotlab:mur(R),
+    N=robotlab:mur(R),
+    ListePortes=[O,S,E,N],
+    % On calcule les processus
+    ListeProcessusVoisins = salles_calcul_processus_voisins(Salle),
+    Voisins = salles_calcul_voisins(ListePortes,ListeProcessusVoisins,[]),
+    %On envoie à la salle la liste de ses voisins
+    Salle ! {voisins,Voisins,ListePortes,self()},
+    receive 
+	{voisins,ok} -> ok
+    end,
+    Leader = salles_calcul_leader(ListePortes,Salle),
+    Synchroniseur ! sync,
+    ok.
 
-robot_premier_tour(IdRobot,NbRobot,Salle) ->	
-	R = robotlab:get_robot(NbRobot),
-	O=robotlab:mur(R),
-	S=robotlab:mur(R),
-	E=robotlab:mur(R),
-	N=robotlab:mur(R),
-	ListePortes=[O,S,E,N],
-	io:format("~w ~w \n",[NbRobot,ListePortes]),
-	Salle ! {portes,ListePortes,self()},
-	receive 
-		{portes,ok} -> ok,io:format("~w OK \n",[NbRobot])
+% Fonction pour calculer les salles voisines de la salle courante
+salles_calcul_processus_voisins(Salle)->
+    case Salle of
+	salle1 ->
+	    ListeProcessusVoisins = [salle0,salle4,salle2,salle0];
+	salle2 ->
+	    ListeProcessusVoisins = [salle1,salle5,salle3,salle0];
+	salle3 ->
+	    ListeProcessusVoisins = [salle2,salle6,salle0,salle0];
+	salle4 ->
+	    ListeProcessusVoisins = [salle0,salle7,salle5,salle1];
+	salle5 ->
+	    ListeProcessusVoisins = [salle4,salle8,salle6,salle2];
+	salle6 ->
+	    ListeProcessusVoisins = [salle5,salle9,salle0,salle3];
+	salle7 ->
+	    ListeProcessusVoisins = [salle0,salle0,salle8,salle4];
+	salle8 ->
+	    ListeProcessusVoisins = [salle7,salle0,salle9,salle5];
+	salle9 ->
+	    ListeProcessusVoisins = [salle8,salle0,salle0,salle6];
+	_ ->
+	    ListeProcessusVoisins = ["Salle non reconnue dans salles_calcul_processus_voisins"]
 	end,
-	Leader = salles_calcul_leader(ListePortes,Salle), 
-	ok.
+    ListeProcessusVoisins.
 
-salles_calcul_leader(ListesPortes,Salle) ->
+
+salles_calcul_voisins([],[],Voisins) ->
+    Voisins;
+% Si on a une salle avec une ouverture vers elle, on la rajoute dans la liste des processus voisins
+salles_calcul_voisins([Porte|ListePortes],[Processus|ListeProcessus],Voisins) ->
+    if
+	(Porte == true) and (Processus /= salle0) ->
+	    salles_calcul_voisins(ListePortes, ListeProcessus, [Processus|Voisins]);
+	true ->
+	    salles_calcul_voisins(ListePortes, ListeProcessus,Voisins)
+    end.
+    
+% Suivant les emplacements des portes et de la salle, on détermine si la salle courante possède la sortie ou non, et donc si elle est le leader
+salles_calcul_leader(ListePortes,Salle) ->
 	case Salle of 
-		salle1 -> Po = lists:nth(1,ListesPortes),
-			    Pn = lists:nth(4,ListesPortes),
+		salle1 -> Po = lists:nth(1,ListePortes),
+			    Pn = lists:nth(4,ListePortes),
 			    if 
 				Po or Pn ->
 					Salle ! {leader,neo};
 			 
   			    true -> Salle ! {leader, nop}
 			    end;
-		salle2 -> Pn = lists:nth(4,ListesPortes),
+		salle2 -> Pn = lists:nth(4,ListePortes),
 			    if 
 				Pn ->
 					Salle ! {leader,neo};
 				true -> Salle ! {leader, nop}
 			    end;
-		salle3 -> Pe = lists:nth(3,ListesPortes),
-			    Pn = lists:nth(4,ListesPortes),
+		salle3 -> Pe = lists:nth(3,ListePortes),
+			    Pn = lists:nth(4,ListePortes),
 			    if 
 				Pe or Pn ->
 					Salle ! {leader,neo};
 			 
   			    true -> Salle ! {leader, nop}
 			    end;
-		salle6 -> Pe = lists:nth(3,ListesPortes),
+		salle6 -> Pe = lists:nth(3,ListePortes),
 			    if 
 				Pe ->
 					Salle ! {leader,neo};
 				true -> Salle ! {leader, nop}
 			    end;
-		salle9 -> Pe = lists:nth(3,ListesPortes),
-			    Ps = lists:nth(2,ListesPortes),
+		salle9 -> Pe = lists:nth(3,ListePortes),
+			    Ps = lists:nth(2,ListePortes),
 			    if 
 				Pe or Ps ->
 					Salle ! {leader,neo};
 			 
   			    true -> Salle ! {leader, nop}
 			    end;
-		salle8 -> Ps = lists:nth(2,ListesPortes),
+		salle8 -> Ps = lists:nth(2,ListePortes),
 			    if 
 				Ps ->
 					Salle ! {leader,neo};
 				true -> Salle ! {leader, nop}
 			    end;
-		salle7 -> Po = lists:nth(1,ListesPortes),
-			    Ps = lists:nth(2,ListesPortes),
+		salle7 -> Po = lists:nth(1,ListePortes),
+			    Ps = lists:nth(2,ListePortes),
 			    if 
 				Po or Ps ->
 					Salle ! {leader,neo};
 			 
   			    true -> Salle ! {leader, nop}
 			    end;
-		salle4 -> Po = lists:nth(1,ListesPortes),
+		salle4 -> Po = lists:nth(1,ListePortes),
 			    if 
 				Po ->
 					Salle ! {leader,neo};
@@ -119,14 +175,60 @@ salles_calcul_leader(ListesPortes,Salle) ->
 			    end;
 		salle5 -> Salle ! {leader,nop}
 
-	end,
-			
-	ok.
-
-
+	end.
 
 salles_construction_arbre() ->
 	ok.
 	
 robot_prochaine_salle(Pid) ->
 	ok.
+
+% Permet de synchroniser les 9 salles ou les 9 robots
+synchroniseur() ->
+    receive
+	sync ->
+	     ok
+    end,
+    receive
+	sync ->
+	     ok
+    end,
+    receive
+	sync ->
+	     ok
+    end,
+    receive
+	sync ->
+	     ok
+    end,
+    receive
+	sync ->
+	     ok
+    end,
+    receive
+	sync ->
+	     ok
+    end,
+    receive
+	sync ->
+	     ok
+    end,
+    receive
+	sync ->
+	     ok
+    end,
+    receive
+	sync ->
+	     ok
+    end,
+    salle1!sync,
+    salle2!sync,
+    salle3!sync,
+    salle4!sync,
+    salle5!sync,
+    salle6!sync,
+    salle7!sync,
+    salle8!sync,
+    salle9!sync,
+    ok.
+    
